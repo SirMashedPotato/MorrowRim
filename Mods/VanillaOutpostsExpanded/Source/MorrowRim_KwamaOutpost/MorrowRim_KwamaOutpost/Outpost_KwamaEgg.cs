16ef4967177e0e3fc27cc02085456982c539db4d
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2021-12-15 08:25:41 +1300

    Initial upload
    Kwama egg outpost, must be built within 5 tiles of a kwama egg mine.

---
+++ Mods/VanillaOutpostsExpanded/Source/MorrowRim_KwamaOutpost/MorrowRim_KwamaOutpost/Outpost_KwamaEgg.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Outposts;
+using MorrowRim;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace MorrowRim_KwamaOutpost
+{
+    public class Outpost_KwamaEgg : Outpost
+    {
+
+        public static int maxDistance = 5;  //make setting in main assembly
+
+        public static float DistanceFromKwamaNest(int tile)
+        {
+            WorldGrid worldGrid = Find.WorldGrid;
+            WorldObjectsHolder worldObjectsHolder = Find.WorldObjects;
+            List<Settlement> kwamaSettlements = new List<Settlement>(worldObjectsHolder.SettlementBases.Where(x => x.Faction.def == MorrowRim.FactionDefOf.MorrowRim_Kwama));
+            float distance = -1f;
+            for (int i = 0; i != kwamaSettlements.Count; i++)
+            {
+                float distance2 = worldGrid.ApproxDistanceInTiles(tile, kwamaSettlements[i].Tile);
+                if (distance == -1 || distance > distance2) distance = distance2;
+            }
+            return distance;
+        }
+
+        public static bool CloseEnough(float distance)
+        {
+            return distance <= maxDistance;
+        }
+
+        public static string CanSpawnOnWith(int tile, List<Pawn> pawns) => !CloseEnough(DistanceFromKwamaNest(tile)) ? "MorrowRimOutposts.MustBeMade.KwamaNest".Translate(maxDistance) : null;
+
+        public static string RequirementsString(int tile, List<Pawn> pawns) =>
+            Requirement("MorrowRimOutposts.MustBeMade.KwamaNest".Translate(maxDistance), CloseEnough(DistanceFromKwamaNest(tile)));
+    }
+}
